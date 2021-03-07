42ce77f547b3139733665a601f520613e79e5880
Sherly Febrianti <sherlyfebrianti96@yahoo.co.id>
2021-03-07 14:03:10 +0100

    initiate the base layout

---
+++ src/components/Body/Body.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Body.css';
+
+class Body extends React.Component {
+  render() {
+    return (
+        <div className="body">
+            This is the body
+        </div>
+    );
+  }
+}
+
+export default Body;
